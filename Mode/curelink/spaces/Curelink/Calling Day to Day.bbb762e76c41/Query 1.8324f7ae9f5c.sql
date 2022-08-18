a8991da552a6b9aa1aa14e1eb1202be5f061f820
Jaideep <jai@cure.link>
2022-08-18 09:21:03 +0000

    [Mode] Update Report 'Calling Day to Day'

--- Mode/curelink/spaces/Curelink/Calling Day to Day.bbb762e76c41/Query 1.8324f7ae9f5c.sql
+++ Mode/curelink/spaces/Curelink/Calling Day to Day.bbb762e76c41/Query 1.8324f7ae9f5c.sql
@@ -1,4 +1,11 @@
-with attempted_calls as ({{ @attempted_calls }})
+with attempted_calls as (
+select owner_id, count(messaging_calllog.id) as call_count 
+from messaging_calllog
+inner join users_doctorprofile on users_doctorprofile.user_id = messaging_calllog.owner_id
+where duration > 60
+and type = 'OUTGOING_TYPE'
+group by owner_id
+)
 ,
 successful_calls as (
 select owner_id, count(messaging_calllog.id) as call_count 
