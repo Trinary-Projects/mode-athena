9c384ce475d897782a1959b3aeae10ec8f1166d0
Jaideep <jai@cure.link>
2022-08-18 08:46:39 +0000

    [Mode] Initialize Mode/GitHub integration

---
+++ Mode/curelink/definitions/attempted_calls.sql
@@ -0,0 +1,5 @@
+select owner_id, count(messaging_calllog.id) as call_count 
+from messaging_calllog
+inner join users_doctorprofile on users_doctorprofile.user_id = messaging_calllog.owner_id
+where type = 'OUTGOING_TYPE'
+group by owner_id
